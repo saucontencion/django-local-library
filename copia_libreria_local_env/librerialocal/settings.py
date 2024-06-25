bbd03f757c3896fcf7b3d6e2e0ed6bdf2ecddd3c
enoc <enoc.alejandro.castrolobo@gmail.com>
2024-06-25 19:43:26 -0400

    contador = 4

--- copia_libreria_local_env/librerialocal/settings.py
+++ copia_libreria_local_env/librerialocal/settings.py
@@ -122,32 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-######
-import os
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Templates configuration
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-SESSION_SAVE_EVERY_REQUEST = True
-# Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
--- librerialocal/catalogo/views.py
+++ librerialocal/catalogo/views.py
@@ -1,3 +1,82 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Genre
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+# def index(request):
+#     return render(request, 'catalogo/index.html')
 
-# Create your views here.
+class GenreListView(ListView):
+    model = Genre
+    template_name = 'catalogo/genre_list.html'
+
+class GenreDetailView(DetailView):
+    model = Genre
+    template_name = 'catalogo/genre_detail.html'
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+     # Calcular el número de géneros
+    num_genres = Genre.objects.count()
+
+    # Calcular el número de libros que contienen una palabra específica (case-insensitive)
+    search_word = request.GET.get('search_word', '')  # Obtener la palabra de búsqueda del parámetro GET
+    num_books_containing_word = Book.objects.filter(title__icontains=search_word).count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_containing_word': num_books_containing_word,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalogo/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
